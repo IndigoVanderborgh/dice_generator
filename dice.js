23c435aa4c47d1daba2946f6819f6f1b970c5bd7
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-02 18:55:43 -0700

    Update dice.js

--- dice.js
+++ dice.js
@@ -15,6 +15,21 @@
 // so taking all of this together, we can make a random number with whole numbers instead
 // of decimels by combining the two ==>
 // let randomNumber = Math.floor(Math.random()*6)
+// This however only adds up to 5, and includes 0, which is not ideal.
+// So we have to shift the addition by one to include 6 and exclude 0. see below
+
+// let randomNumber = Math.floor(Math.random()*6) + 1
+
 // console.log(randomNumber)
 
+// lets create a function, rollDice(), that returns a random number between 1 and 6 now
 
+function rollDice() {
+    let randomNumber = Math.floor(Math.random() + 6) + 1
+    return randomNumber
+}
+
+
+//  Just to check, always remember to console.log
+
+console.log(rollDice());
