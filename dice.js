7feb0c13a5bc261f0d8b9d58c26f76e5a157111f
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-02 18:41:07 -0700

    Initial commit

---
+++ dice.js
@@ -0,0 +1,20 @@
+// random numbers in Javascript basics:
+
+// Math.random() will provide a random number rangin from 0 (including 0) and 1 (not including 1)
+
+// let randomNumber = Math.random() * 6 will provide the range of 0 - 5.999
+
+// console.log(randomNumber)
+
+// Math.floor takes some input and registers it as a whole number, ignoring the decimels.
+
+// let flooredNumber = Math.floor(3.45632)
+
+// console.log(flooredNumber) ==> 3
+
+// so taking all of this together, we can make a random number with whole numbers instead
+// of decimels by combining the two ==>
+// let randomNumber = Math.floor(Math.random()*6)
+// console.log(randomNumber)
+
+
